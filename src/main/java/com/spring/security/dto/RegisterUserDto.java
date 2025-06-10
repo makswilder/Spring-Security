fd4afa403c948b2502bf94af6d613ce7f02a188e
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2025-06-10 18:08:34 +0200

    Created Dtos LoginUserDto, RegisterUserDto and VerifyUserDto

---
+++ src/main/java/com/spring/security/dto/RegisterUserDto.java
@@ -0,0 +1,12 @@
+package com.spring.security.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String username;
+}
